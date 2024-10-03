e16d9cc3cb2b297c4a0b4e031bd7ea134cac4a76
Jesse-Tingle <jtingle0@gmail.com>
2024-10-02 21:15:00 -0400

    adds html and css for contest section, adds js file with initial funtion to pick user hand

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const pickUserHand = (hand) => {
+	console.log(hand)
+	// hide the current page
+	// show the next page with the hand you picked
+	let hands = document.querySelector(".hands")
+	hands.style.display = "none"
+}
