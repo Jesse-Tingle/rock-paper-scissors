cec26fadf4bea5a9003a5daef3988e32ea6f7c52
Jesse-Tingle <jtingle0@gmail.com>
2024-10-02 21:35:38 -0400

    adds js to make user selection appear on screen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+const handOptions = {
+	rock: "/images/Rock.png",
+	paper: "/images/Paper.png",
+	scissors: "/images/Scissors.png",
+}
+
 const pickUserHand = (hand) => {
 	console.log(hand)
 	// hide the current page
 	// show the next page with the hand you picked
 	let hands = document.querySelector(".hands")
 	hands.style.display = "none"
+
+	//show the next page with the hand you picked
+	let contest = document.querySelector(".contest")
+	contest.style.display = "flex"
+
+	//set the user pick
+
+	document.getElementById("userPickImage").src = handOptions[hand]
 }
